3214140566687edc3d183a560bbdb3f8af16ecf1
user <juravski95@gmail.com>
2018-11-24 14:46:44 +0200

    predict single x

--- ass2/tagger1.py
+++ ass2/tagger1.py
@@ -3,11 +3,12 @@ import sys
 import numpy as np
 import random
 
+
 word2vec = None
 tag_set = None
 
 N1 = 500
-EPOCHS = 10
+EPOCHS = 5
 BATCH_SIZE = 1000
 
 
@@ -141,6 +142,7 @@ def train_model(sen_arr, vocab, tag_set):
 
     # define the parameters
     m = dy.ParameterCollection()
+
     w1 = m.add_parameters((N1, 250))
     w2 = m.add_parameters((len(tag_set), N1))
     b1 = m.add_parameters((N1))
@@ -148,15 +150,16 @@ def train_model(sen_arr, vocab, tag_set):
     E = m.add_lookup_parameters((len(vocab), 50), init='uniform', scale=(np.sqrt(6)/np.sqrt(50)))
 
     # create trainer
-    trainer = dy.SimpleSGDTrainer(m)
+    #trainer = dy.SimpleSGDTrainer(m)
+    trainer = dy.AdamTrainer(m)
 
     total_loss = 0
     seen_instances = 0
 
     for epoch in range(EPOCHS):
         random.shuffle(sen_arr)
-        for sen_i in range(BATCH_SIZE):
-            sen = sen_arr[sen_i]
+        for sen in sen_arr:
+            #sen = sen_arr[sen_i]
             for i in range(len(sen)):
                 dy.renew_cg()
                 word, tag = sen[i].split()
@@ -181,19 +184,33 @@ def train_model(sen_arr, vocab, tag_set):
             if sen_arr.index(sen) % 100 ==0:
                 print str(sen_arr.index(sen)) + "/" + str(len(sen_arr))
 
+    return (w1, w2, b1, b2, E)
 
 
+def predict_tag(params):
+    (w1, w2, b1, b2, E) = params
+    dy.renew_cg()
+    inputs = [100,101,102,103,104]
+
+    emb_vectors = [E[i] for i in inputs]
+    net_input = dy.concatenate(emb_vectors)
+    l1 = dy.tanh((w1 * net_input) + b1)
+    net_output = (w2 * l1) + b2
+
+    return np.argmax(net_output.npvalue())
 
 if __name__ == '__main__':
 
     train_data = sys.argv[1]
-    #vocab_data = sys.argv[2]
-    #wordVectors_data = sys.argv[3]
 
-    #word2vec = createWordVecDict(train_data)
     vocab = getDataVocab(train_data)
     sen_arr, tag_set = load_train_data(train_data)
 
-    train_model(sen_arr, vocab, tag_set)
+    (w1, w2, b1, b2, E) = train_model(sen_arr, vocab, tag_set)
+    tag_hat = predict_tag((w1, w2, b1, b2, E))
+
+    for name, age in tag_set.items():
+        if age == tag_hat:
+            print(name)
 
     pass
