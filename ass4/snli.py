25f79310ba35ead918daddaf54cb9ab7c1aa2672
user <juravski95@gmail.com>
2018-12-26 11:06:50 +0200

    train done

--- ass4/snli.py
+++ ass4/snli.py
@@ -3,21 +3,33 @@ import dynet as dy
 import numpy as np
 from random import randint
 from random import shuffle
+import datetime
 
-EPOCHS = 1
 
 LEN_EMB_VECTOR = load_data.LEN_EMB_VECTOR
 NUM_OF_OOV_EMBEDDINGS = load_data.NUM_OF_OOV_EMBEDDINGS
 OOV_EMBEDDING_STR = load_data.OOV_EMBEDDING_STR
 
+EPOCHS = 1
+LR = 0.005
+
 F_INPUT_SIZE = LEN_EMB_VECTOR
 F_HIDDEN_SIZE = 200
 F_OUTPUT_SIZE = 200
+
+G_INPUT_SIZE = 2*LEN_EMB_VECTOR
+G_HIDDEN_SIZE = 200
+G_OUTPUT_SIZE = 200
+
+H_INPUT_SIZE = 2*G_OUTPUT_SIZE
+H_HIDDEN_SIZE = 200
+H_OUTPUT_SIZE = 3
 
 
 
 def init_model():
     model = dy.ParameterCollection()
+    trainer = dy.AdagradTrainer(model, learning_rate=LR)
     model_params = {}
 
     # F feed-forward
@@ -31,9 +43,29 @@ def init_model():
     model_params['F_w2'] = F_w2
     model_params['F_b2'] = F_b2
 
+    # G feed-forward
+    G_w1 = model.add_parameters((G_HIDDEN_SIZE, G_INPUT_SIZE))
+    G_b1 = model.add_parameters((G_HIDDEN_SIZE))
+    G_w2 = model.add_parameters((G_OUTPUT_SIZE, G_HIDDEN_SIZE))
+    G_b2 = model.add_parameters((G_OUTPUT_SIZE))
 
+    model_params['G_w1'] = G_w1
+    model_params['G_b1'] = G_b1
+    model_params['G_w2'] = G_w2
+    model_params['G_b2'] = G_b2
 
-    return model, model_params
+    # H feed-forward
+    H_w1 = model.add_parameters((H_HIDDEN_SIZE, H_INPUT_SIZE))
+    H_b1 = model.add_parameters((H_HIDDEN_SIZE))
+    H_w2 = model.add_parameters((H_OUTPUT_SIZE, H_HIDDEN_SIZE))
+    H_b2 = model.add_parameters((H_OUTPUT_SIZE))
+
+    model_params['H_w1'] = H_w1
+    model_params['H_b1'] = H_b1
+    model_params['H_w2'] = H_w2
+    model_params['H_b2'] = H_b2
+
+    return model, model_params, trainer
 
 
 def get_x_y(sample):
@@ -148,11 +180,85 @@ def get_alpha_beta(E_matrix, len_cols, len_rows, sen1, sen2, emb_data):
     return alpha, beta
 
 
+def get_v1_v2(beta, alpha, sen1, sen2, len_sen1, len_sen2, emb_data, model, model_params):
+    """
+
+    :param beta:
+    :param alpha:
+    :param sen1:
+    :param sen2:
+    :param len_sen1:
+    :param len_sen2:
+    :param emb_data:
+    :param model:
+    :param model_params:
+    :return: 2 lists of dynet expressions
+    """
+    G_w1 = model_params['G_w1']
+    G_b1 = model_params['G_b1']
+    G_w2 = model_params['G_w2']
+    G_b2 = model_params['G_b2']
+
+    v1 = []
+    for i in range(len_sen1):
+        a_i = get_word_from_dict(sen1[i], emb_data)
+        beta_i = beta[i]
+        con = a_i + beta_i
+        G_x = dy.vecInput(G_INPUT_SIZE)
+        G_x.set(con)
+        G_i = (G_w2 * (dy.rectify(G_w1 * G_x + G_b1)) + G_b2)
+        v1.append(G_i)
+
+    v2 = []
+    for j in range(len_sen2):
+        b_j = get_word_from_dict(sen2[j], emb_data)
+        alpha_j = alpha[j]
+        con = b_j + alpha_j
+        G_x = dy.vecInput(G_INPUT_SIZE)
+        G_x.set(con)
+        G_i = (G_w2 * (dy.rectify(G_w1 * G_x + G_b1)) + G_b2)
+        v2.append(G_i)
+
+    return v1, v2
 
 
-def train_model(train_data, dev_data, emb_data, model, model_params):
+def aggregate_v1_v2(v1, v2, model, model_params):
+    """
+
+    :param v1:
+    :param v2:
+    :param model:
+    :param model_params:
+    :return: y_hat softmaxed dynet expression
+    """
+    H_w1 = model_params['H_w1']
+    H_b1 = model_params['H_b1']
+    H_w2 = model_params['H_w2']
+    H_b2 = model_params['H_b2']
+
+    v1_esum = dy.esum(v1)
+    v1_sum = v1_esum.value()
+
+    v2_esum = dy.esum(v2)
+    v2_sum = v2_esum.value()
+
+    con = v1_sum + v2_sum
+
+    H_x = dy.vecInput(H_INPUT_SIZE)
+    H_x.set(con)
+
+    y_hat = dy.softmax(H_w2 * (dy.rectify(H_w1 * H_x + H_b1)) + H_b2)
+
+    return y_hat
+
+
+
+def train_model(train_data, dev_data, emb_data, model, model_params, trainer):
     for epoch_i in range(EPOCHS):
         shuffle(train_data)
+        print("Epoch " + str(epoch_i + 1) + " started at: " + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+        correct = wrong = 0.0
+        total_loss = 0
         for sample_i in range(len(train_data)):
             #print sample_i
             dy.renew_cg()
@@ -160,10 +266,31 @@ def train_model(train_data, dev_data, emb_data, model, model_params):
             sen1, sen2, label = get_x_y(sample)
             len_sen1 = len(sen1)
             len_sen2 = len(sen2)
+            #y = dy.scalarInput(0)
+            #y.set(label)
 
             E_matrix = set_E_matrix(sen1, sen2, len_sen1, len_sen2, model, model_params, emb_data)
             alpha, beta = get_alpha_beta(E_matrix, len_sen1, len_sen2, sen1, sen2, emb_data)
-            pass
+            v1, v2 = get_v1_v2(alpha, beta, sen1, sen2, len_sen1, len_sen2, emb_data, model, model_params)
+            y_hat_vec_expression = aggregate_v1_v2(v1, v2, model, model_params)
+
+            loss = -(dy.log(dy.pick(y_hat_vec_expression, label)))
+            loss_val = loss.value()
+            loss.backward()
+            trainer.update()
+
+            total_loss += loss_val
+            label_hat = np.argmax(y_hat_vec_expression.npvalue())
+            if label_hat == label:
+                correct += 1
+            else:
+                wrong += 1
+
+            if sample_i % 100 == 0 and (sample_i > 0):
+                acc = (correct /(correct+wrong)) * 100
+                relative_total_loss = total_loss/sample_i
+                print("Epoch %d: Train iteration %d: loss=%.4f acc=%%%.2f" % (epoch_i+1, sample_i, relative_total_loss, acc))
+
 
 
 
@@ -182,8 +309,8 @@ if __name__ == '__main__':
     emb_data = load_data.get_emb_data(glove_emb_file)
 
 
-    model, model_params = init_model()
-    train_model(train_data, dev_data, emb_data, model, model_params)
+    model, model_params, trainer = init_model()
+    train_model(train_data, dev_data, emb_data, model, model_params, trainer)
 
 
     pass
